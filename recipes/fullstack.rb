e4e069a1e95acbe6b69f550582f554ad5295d629
Jeff Siegel <jdsiegel@gmail.com>
2013-03-03 18:34:59 -0700

    add default process list for fullstack

--- recipes/fullstack.rb
+++ recipes/fullstack.rb
@@ -26,7 +26,7 @@ node['orch']['apps'].each do |app|
     'user'         => app['user'],
     'port'         => app.fetch('port') { 5000 },
     'ruby_version' => app['ruby_version'],
-    'processes'    => app['processes'],
+    'processes'    => app.fetch('processes') { [['all', 1]] },
     'environment'  => app['environment']
   }
 
