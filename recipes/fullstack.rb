f4b5862d5caf3f555912e2a0e31dcea37d199c35
Jeff Siegel <jdsiegel@gmail.com>
2013-03-03 17:40:37 -0700

    add default value for root path
    This will be the most common path for an app's root directory. The
    option to override it remains

--- recipes/fullstack.rb
+++ recipes/fullstack.rb
@@ -15,7 +15,7 @@ node['orch']['apps'].each do |app|
 
   web = {
     'name'      => app['name'],
-    'root_path' => app['root_path'],
+    'root_path' => app.fetch('root_path') { "/home/#{app['user']}/app/current/public" },
     'servers'   => app['servers']
   }
 
