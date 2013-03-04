23c431c62149891dd36f579e4609c4724d9e1b7f
Jeff Siegel <jdsiegel@gmail.com>
2013-03-03 17:24:08 -0700

    test to ensure static files are served by nginx

--- test/integration/fullstack/shunit2/app_test.sh
+++ test/integration/fullstack/shunit2/app_test.sh
@@ -4,3 +4,11 @@ test_app_is_running_on_port_80()
 
   assertEquals "If you see this, the app is working." "$output"
 }
+
+test_static_files_are_served()
+{
+  local output=$(curl -s http://localhost/robots.txt)
+  IFS=$'\n' lines=($output)
+
+  assertEquals "User-Agent: *" "${lines[0]}"
+}
