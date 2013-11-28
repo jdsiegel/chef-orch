aeb96ceed1c6f87d16854850d225c18a4aa33829
Jeff Siegel <jdsiegel@gmail.com>
2013-11-27 19:11:16 -0700

    enabled more web options to orch_web

--- recipes/web.rb
+++ recipes/web.rb
@@ -2,10 +2,13 @@ node.override['orch_web']['apps'] = []
 
 node['orch']['apps'].each do |app|
   web = {
-    'name'      => app['name'],
-    'root_path' => app.fetch('root_path') { "/home/#{app['user']}/app/current/public" },
-    'servers'   => app['servers'],
-    'ssl'       => app['ssl']
+    'name'           => app['name'],
+    'root_path'      => app.fetch('root_path') { "/home/#{app['user']}/app/current/public" },
+    'hostname'       => app['hostname'],
+    'port'           => app['port'],
+    'servers'        => app['servers'],
+    'ssl'            => app['ssl'],
+    'asset_location' => app['asset_location']
   }
 
   node.override['orch_web']['apps'] << web
